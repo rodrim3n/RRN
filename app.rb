07afe4fd324d7d448646e334857035a521bce75c
Rodrigo Otero <rodri.m3n@gmail.com>
2015-09-09 16:15:30 -0300

    adds security layer

--- app.rb
+++ app.rb
@@ -1,26 +1,26 @@
-require "cuba"
 require "cuba/render"
 require "cuba/safe"
-require "mote"
-require "mote/render"
-require "rack/protection"
-require "sequel"
-require "mysql"
 
 Cuba.plugin(Cuba::Render)
 Cuba.plugin(Mote::Render)
 Cuba.plugin(Cuba::Safe)
 
-
 Cuba.use(Rack::Session::Cookie, :secret => "!@(!P{($)})")
 Cuba.use(Rack::Protection)
-Cuba.use (Rack::MethodOverride)
+Cuba.use(Rack::MethodOverride)
 
 Dir["./config/*.rb"].each {|f| require(f) }
 Dir["./routes/*.rb"].each { |f| require(f) }
 Dir["./models/*.rb"].each { |f| require(f) }
 
 Cuba.define do
+  on csrf.unsafe? do
+    csrf.reset!
+    res.status = 403
+    res.write("Not authorized")
+    halt(res.finish)
+  end
+
   on root do
     render("home")
   end
