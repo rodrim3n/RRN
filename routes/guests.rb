0e5c620af32254c292182f611924aee22f026468
Rodrigo Otero <rodri.m3n@gmail.com>
2015-09-10 23:54:27 -0300

    adds login functionality & password crypt

--- routes/guests.rb
+++ routes/guests.rb
@@ -1,11 +1,14 @@
 class Guests < Cuba
   define do
     on get, "signup", !authenticated(User) do
-      render("signup", msg: "")
+      render("guests/signup", msg: "")
     end
 
     on post, "signup", param("username"), param("password") do |u, p|
-      user = User.new(:username => u, :crypted_password => p)
+      # user = User.new(:username => u, :crypted_password => p)
+      user = User.new
+      user.username= u
+      user.password= p
       begin
         if user.save
           authenticate(user)
@@ -13,28 +16,25 @@ class Guests < Cuba
           render("home", msg: session[:success])
         end
       rescue Sequel::ValidationFailed
-        session[:error] = user.errors
-        render("signup", msg: session[:error])
+        session[:error] = "Username already in use."
+        render("guests/signup", msg: session[:error])
       end
     end
 
     on get, "login" do
       on get do
-        render("login")
+        render("guests/login")
       end
     end
 
-  #   on post, "login", param("username"), param("password") do |u, p|
-  #     if login(User, u, p)
-  #       session[:success] = "You have successfully logged in."
-  #       res.redirect("/admin")
-  #       p "te logueaste"
-  #     else
-  #       session[:error] = "Invalid username and/or password combination."
-  #       p "no pudiste loguearte"
-  #     end
-  #   end
-  # end
-
+    on post, "login", param("username"), param("password") do |u, p|
+      if login(User, u, p)
+        session[:success] = "You have successfully logged in."
+        render("admins/index", msg: session[:success])
+      else
+        session[:error] = "Invalid username and/or password combination."
+        render("guests/login", msg: session[:error])
+      end
+    end
   end
 end
