3d87bfd6625cc261e6cd85d1abea90056ba718ea
Rodrigo Otero <rodri.m3n@gmail.com>
2015-09-12 13:58:02 -0300

    adds simple admin crud

--- routes/admins.rb
+++ routes/admins.rb
@@ -1,17 +1,17 @@
 class Admins < Cuba
   define do
 
-    on get do
+    on get, authenticated(User) do
       on root do
-        render("admins/index")
+        render "admins/index"
       end
 
       on "new" do #new
-        render "admin/new"
+        render "admins/new"
       end
 
       on "list" do
-        render("admins/list", admins: User.all)
+        render "admins/list", admins: User.all
       end
 
       on "logout" do
@@ -19,31 +19,54 @@ class Admins < Cuba
         session[:success] = "You have successfully logged out."
         res.redirect "/", 302
       end
+
+      on "edit/:id" do |id|
+        render("admins/edit", admin: User.find(:id => id))
+      end
     end
 
-    on post do
+    on post, authenticated(User) do
       on root, param("username"), param("password") do |u, p| #create
         user = User.new
         user.username= u
         user.password= p
         begin
           if user.save
-            session[:success] = "New admin created successfully."
+            session[:success] = "New admin reated successfully."
             res.redirect "/admin", 302
           end
         rescue Sequel::ValidationFailed
           session[:error] = "Username already in use."
-          render("admin/new")# msg: session[:error])
+          res.redirect "admin/new"
         end
       end
     end
 
-    on delete do
-      pass
+    on put, authenticated(User) do
+      on ":id", param("oldpassword"), param("newpassword") do |id, old, new| #Update
+        user = User.find(:id => id)
+        if (Shield::Password.check(old, user.crypted_password))
+          user.password = new
+          user.save_changes
+          session[:success] = "You have updated #{user.username} password."
+          res.redirect "/admin/list", 302
+        else
+          session[:error] = "Old password doesn't match with new."
+          res.redirect "/admin/edit/#{user.id}", 302
+        end
+      end
     end
 
-    on put do
-      pass
+    on delete, authenticated(User) do #Destroy
+      on "delete/:id" do |id|
+        User.find(:id => id).destroy
+        res.redirect "/admin/list", 302
+      end
+    end
+
+    on default do
+      res.status = 403
+      res.write "403 - Forbidden."
     end
   end
 end
