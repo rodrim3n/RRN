2c2f1b8f4250dd0fd7b9c9f7a7e44c552146ebae
Rodrigo Otero <rodri.m3n@gmail.com>
2015-09-16 21:25:45 -0300

    adds upload_file helper

---
+++ lib/helpers.rb
@@ -0,0 +1,27 @@
+module Helpers
+  def current_user
+    authenticated(User)
+  end
+
+  def notfound(msg)
+    res.status = 404
+    res.write(msg)
+    halt(res.finish)
+  end
+
+  def forbidden(msg)
+    res.status = 403
+    res.write(msg)
+    halt(res.finish)
+  end
+
+  def file_uploader(file)
+    tempfile = file[:tempfile]
+    filename = SecureRandom.hex(10) + file[:filename]
+    File.open("./public/img/#{filename}", 'wb') do |f|
+      f.write(tempfile.read)
+    end
+    filename
+  end
+
+end
